d32101096160d9c2b26353dcfb03dc29be7e08de
Reedwarn <ridwankolawole93@gmail.com>
2023-07-14 01:52:40 +0100

    Initial commit

--- contracts/gasChallenge.sol
+++ contracts/gasChallenge.sol
@@ -2,11 +2,8 @@
 pragma solidity ^0.8.0;
 
 contract gasChallenge {
-    //Implement Fixed-Size Array Technique Here
-    uint[] numbers = [1,2,3,4,5,6,7,8,9,10];
+    uint[10] numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
     
-    //Function to check for sum of array
-    //No changes required in this function
     function getSumOfArray() public view returns (uint256) {
         uint sum = 0;
         for (uint i = 0; i < numbers.length; i++) {
@@ -21,9 +18,13 @@ contract gasChallenge {
         }
     }
     
-    //Implement Remaining Gas Optimization Techniques Here
-    //Sum of elements in the numbers array should equal 0
     function optimizedFunction() public {
-      
+        uint[10] storage numRef = numbers; // Caching state variable
+        unchecked { // Implement unchecked block
+            for (uint i = 0; i < numRef.length; ++i) {
+                numRef[i] = 0;
+            }
+        }
+
     }
- }
+}
